a942f025cf05f7a877566577a05b39cf2e6fa229
Salinakasa <salina.kansakar@webo.digital>
2024-01-18 10:16:59 +0545

    Feat: verifying page added

--- app/routes/_public.otp-verification/route.tsx
+++ app/routes/_public.otp-verification/route.tsx
@@ -1,7 +1,8 @@
-import { Form, useActionData } from '@remix-run/react';
+import { Form, Link, useActionData } from '@remix-run/react';
 import { ActionFunctionArgs } from '@remix-run/server-runtime';
 import { useState, Fragment, useRef, useEffect } from 'react';
 import { DangerAlert } from '~/components/icons/alert';
+import { ArrowLeftSmall } from '~/components/icons/arrowleft';
 import { Button } from '~/components/ui/button';
 
 let currentOtpIndex: number = 0;
@@ -36,8 +37,10 @@ type ActionData = {
 const OtpVerification = () => {
   const [tempOtp, setTempOtp] = useState<string[]>(new Array(5).fill(''));
   const [activeOtpIndex, setActiveOtpIndex] = useState<number>(0);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState(false);
   let [otp, setOtp] = useState<number>(0);
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleOtpChange = (value: number) => {
     setOtp(value);
@@ -77,36 +80,52 @@ const OtpVerification = () => {
   const actionData = useActionData<ActionData>();
   console.log('actionData', actionData);
   return (
-    <Form method="post">
-      <div className="flex items-center space-x-2 w-fit">
-        {tempOtp.map((_, index) => {
-          return (
-            <Fragment key={index}>
-              <input
-                ref={index === activeOtpIndex ? inputRef : null}
-                onChange={handleOnchange}
-                onKeyDown={(e) => handleOnKeyDown(e, index)}
-                className={`w-[54px] aspect-square block appearance-none text-center otp-field`}
-                type="number"
-                name="otp"
-                value={tempOtp[index]}
-                min={1}
-              // required
-              />
-            </Fragment>
-          );
-        })}
+    <div className='md:w-[398px] w-full'>
+      <div className="flex flex-col p-8 space-y-8 bg-white shadow-3xl">
+        <div className="flex flex-col items-center justify-center gap-y-5">
+          <h4>Enter your code!</h4>
+          <p>We have just sent code to your email address</p>
+        </div>
+        <Form method="post" className='flex flex-col gap-y-6'>
+          <div>
+            <div className="flex items-center space-x-4 w-fit">
+              {tempOtp.map((_, index) => {
+                return (
+                  <Fragment key={index}>
+                    <input
+                      ref={index === activeOtpIndex ? inputRef : null}
+                      onChange={handleOnchange}
+                      onKeyDown={(e) => handleOnKeyDown(e, index)}
+                      className={`max-w-[54px] aspect-square block appearance-none text-center otp-field`}
+                      type="number"
+                      name="otp"
+                      value={tempOtp[index]}
+                      min={1}
+                    />
+                  </Fragment>
+                );
+              })}
+            </div>
+            {actionData?.formErrors?.otp ? (
+              <p className="pt-1 error-msg">
+                <DangerAlert />
+                <span className="pl-2">{actionData?.formErrors?.otp}</span>
+              </p>
+            ) : null}
+          </div>
+          <Button variant="primary" type="submit">
+            VERIFY
+          </Button>
+        </Form>
       </div>
-      {actionData?.formErrors?.otp ? (
-        <p className="pt-1 error-msg">
-          <DangerAlert />
-          <span className="pl-2">{actionData?.formErrors?.otp}</span>
-        </p>
-      ) : null}
-      <Button size="small" type="submit">
-        Create Account
-      </Button>
-    </Form>
+      <div className='mt-4 space-y-6 text-center'>
+        <p>Didn’t receive code? <Link to="#" className='underline text-primary-500'>Click to resend</Link></p>
+        <Link to="/login" className='inline-flex items-center gap-3 px-6 py-2 text-sm italic font-bold uppercase border border-solid border-primary-500'>
+          <ArrowLeftSmall /> back to log in
+        </Link>
+      </div>
+    </div>
+
   );
 }
 
