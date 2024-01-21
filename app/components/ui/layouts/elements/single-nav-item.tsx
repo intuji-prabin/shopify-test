7e77073137b510a4cb6a17558202f80e7ecb4bb6
Binisha <binisha.bajracharya@webo.digital>
2024-01-21 20:32:24 +0545

    fix:submeu hover megamenu fixed

--- app/components/ui/layouts/elements/single-nav-item.tsx
+++ app/components/ui/layouts/elements/single-nav-item.tsx
@@ -2,22 +2,26 @@ import {useEffect, useRef, useState} from 'react';
 import {MenuItems} from '../bottom-header';
 import ArrowUp from '~/components/icons/arrowUp';
 import ArrowDown from '~/components/icons/arrowDown';
-import {Link} from '@remix-run/react';
+import {Link, useNavigate} from '@remix-run/react';
 import {DropdownMenu} from './dropdownItems';
 
 export const SingleNavItem = ({
   menu,
   depthLevel,
+  activeMenu,
+  setActiveMenu,
 }: {
   menu: MenuItems;
   depthLevel: number;
+  activeMenu: string;
+  setActiveMenu: React.Dispatch<React.SetStateAction<string>>;
 }) => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const menuRef = useRef<HTMLLIElement>(null);
+  const navigate = useNavigate();
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        setIsDropdownOpen(false);
+        setActiveMenu('');
       }
     };
     // Bind the event listener
@@ -39,7 +43,9 @@ export const SingleNavItem = ({
         ref={menuRef}
       >
         <Link to="" className="flex items-center gap-1 relative menu-links">
-          <div className={`${isDropdownOpen ? 'active' : ''} menu-icon`}>
+          <div
+            className={`${activeMenu === menu.title ? 'active' : ''} menu-icon`}
+          >
             {menu.icon}
           </div>
           {menu.submenu ? (
@@ -48,28 +54,30 @@ export const SingleNavItem = ({
                 type="button"
                 aria-haspopup="menu"
                 className={`flex gap-2 items-center font-bold italic text-lg arrow-toggle  focus:outline-none ${
-                  isDropdownOpen ? 'text-secondary-500' : ''
+                  activeMenu === menu.title ? 'text-secondary-500' : ''
                 }`}
-                aria-expanded={isDropdownOpen ? 'true' : 'false'}
+                aria-expanded={activeMenu === menu.title ? 'true' : 'false'}
                 onClick={() => {
                   if (menu.title === 'Product') {
+                    //Bug: navigate not working here
+                    // navigate('/products');
+                    setActiveMenu('');
                     return;
                   }
-                  setIsDropdownOpen(!isDropdownOpen);
+                  setActiveMenu(menu.title);
                 }}
                 onMouseEnter={() => {
                   if (menu.title === 'Product') {
-                    setIsDropdownOpen(true);
-                  }
-                }}
-                onMouseLeave={() => {
-                  if (menu.title === 'Product') {
-                    setIsDropdownOpen(false);
+                    setActiveMenu(menu.title);
                   }
                 }}
               >
-                {menu.title}{' '}
-                {isDropdownOpen ? (
+                {menu.title === 'Product' ? (
+                  <Link to="/products">{menu.title}</Link>
+                ) : (
+                  <>{menu.title} </>
+                )}
+                {activeMenu === menu.title ? (
                   <ArrowUp fillColor="#FFE600" />
                 ) : (
                   <ArrowDown />
@@ -78,13 +86,13 @@ export const SingleNavItem = ({
 
               <DropdownMenu
                 submenus={menu.submenu}
-                isOpen={isDropdownOpen}
+                isOpen={activeMenu === menu.title}
                 depthLevel={depthLevel}
                 type={menu.type === 'megamenu' ? 'megamenu' : 'normal'}
               />
             </>
           ) : (
-            <Link to="">{menu.title}</Link>
+            <Link to={menu.url}>{menu.title}</Link>
           )}
         </Link>
       </li>
