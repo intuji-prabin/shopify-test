9990d65de2caf0dc162a42b4b5aefa26364d68c0
binisha9AA <binisha.bajracharya@intuji.com>
2024-01-29 16:26:31 +0545

    feat:add link to the bottom header menus

--- app/routes/_app.company-profile/company-profile-detail.tsx
+++ app/routes/_app.company-profile/company-profile-detail.tsx
@@ -12,12 +12,12 @@ export default function CompanyProfileDetail({ data }: { data: Data }) {
   const metaNode = companyNode?.metafields?.nodes.length > 0 ? companyNode.metafields.nodes.filter((meta: any) => meta.key === 'fax')[0].value : null;
   const locationNode = companyNode?.locations?.nodes[0];
   return (
-    <div className="flex flex-col justify-between gap-10 p-6 bg-white setting-card-info lg:flex-row lg:gap-0">
+    <div className="flex flex-col justify-between gap-10 p-6 bg-white setting-card-info lg:flex-row lg:gap-6">
       <div className="flex flex-col gap-6 max-w-[unset] lg:max-w-[650px] xl:max-w-[805px]">
         <div className="">
           <div className="flex items-center gap-4">
             <figure className="w-[96px] h-[96px] flex items-center justify-center rounded-[50%] border border-grey-50">
-              <img src="AncLogo.png" alt="" />
+              <img src={distributorLogo} alt="distributor company logo" />
             </figure>
             <h3>{companyNode?.name}</h3>
           </div>
@@ -48,9 +48,9 @@ export default function CompanyProfileDetail({ data }: { data: Data }) {
         </div>
       </div>
 
-      <div className="flex flex-col gap-4 p-6  bg-primary-50">
+      <div className="flex flex-col gap-4 p-6  bg-primary-50 flex-grow">
         <h5 className="text-lg italic font-bold leading-6">
-          Preferred Inventory Location
+          {preferedLocation}
         </h5>
         <ul className="grid grid-cols-1 text-lg font-medium gap-x-10 gap-y-4 text-grey-700 xl:grid-cols-2">
           <li className="settings-card-detail ">
@@ -61,6 +61,7 @@ export default function CompanyProfileDetail({ data }: { data: Data }) {
             <LocationsMinor /> {locationNode?.shippingAddress?.address1}
           </li>
           <li className="settings-card-detail ">
+            {' '}
             <PhoneMajor />
             {locationNode?.shippingAddress?.phone}
           </li>
