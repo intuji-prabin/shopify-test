7065defecff98fab7ca0cc25659d2e6c19b19b99
binisha9AA <binisha.bajracharya@intuji.com>
2024-02-05 16:35:14 +0545

    feat:worked on product comparison page

---
+++ app/routes/_app.product-comparison/comparison-items.tsx
@@ -0,0 +1,27 @@
+import CompareProductSearch from './compare-product-search';
+import CompareDefaultItem from './comparison-default-item';
+
+export default function ComparisonItems() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-[18px] p-6 card-wrapper ">
+      <CompareDefaultItem
+        imageUrl={'product.png'}
+        productName={
+          'ProLite Auto-Darkening Welding Helmet – Terra – 100 Years Of'
+        }
+        buyPrice={0}
+        rppPrice={0}
+        isFavourited={true}
+      />
+      <CompareProductSearch
+        imageUrl={'product.png'}
+        productName={
+          'ProLite Auto-Darkening Welding Helmet – Terra – 100 Years Of'
+        }
+        buyPrice={0}
+        rppPrice={0}
+        isFavourited={true}
+      />
+    </div>
+  );
+}
