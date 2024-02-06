b34a652c9965af5b1df041ebb8b33fce53aa47b1
binisha9AA <binisha.bajracharya@intuji.com>
2024-02-06 12:33:49 +0545

    feat:changed title of company settings to company information

--- app/routes/_app.company-info/route.tsx
+++ app/routes/_app.company-info/route.tsx
@@ -1,26 +1,34 @@
-import { Link } from '@remix-run/react';
-import { settingCards } from './settingCards';
-import { LoaderFunctionArgs } from '@remix-run/server-runtime';
-import { isAuthenticate } from '~/lib/utils/authsession.server';
+import {Link} from '@remix-run/react';
+import {settingCards} from './settingCards';
+import {LoaderFunctionArgs} from '@remix-run/server-runtime';
+import {isAuthenticate} from '~/lib/utils/authsession.server';
 
-export async function loader({ context }: LoaderFunctionArgs) {
+export async function loader({context}: LoaderFunctionArgs) {
   await isAuthenticate(context);
-  return {}
+  return {};
 }
 
 export default function CompanySettings() {
   return (
     <div className="bg-primary-25 container py-12 ">
-      <h3 className="mb-[42px] text-grey-900">Company Settings</h3>
+      <h3 className="mb-[42px] text-grey-900">Company Information</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
         {settingCards.map((settingCard) => (
-          <div key={settingCard.id} className="p-6 bg-white max-w-[627px] flex gap-[35px] flex-col">
+          <div
+            key={settingCard.id}
+            className="p-6 bg-white max-w-[627px] flex gap-[35px] flex-col"
+          >
             <div className="flex flex-col gap-[35px]" key={settingCard.id}>
               <div className="flex  gap-2 flex-col ">
                 <h4>{settingCard.title}</h4>
                 <p>{settingCard.description}</p>
               </div>
-              <Link className="uppercase text-sm italic font-bold py-2.5 px-6 bg-primary-500 text-white max-w-[116px] text-center inline-block" to={settingCard.link}>{settingCard.btnText}</Link>
+              <Link
+                className="uppercase text-sm italic font-bold py-2.5 px-6 bg-primary-500 text-white max-w-[116px] text-center inline-block"
+                to={settingCard.link}
+              >
+                {settingCard.btnText}
+              </Link>
             </div>
           </div>
         ))}
--- app/routes/_app/route.tsx
+++ app/routes/_app/route.tsx
@@ -5,48 +5,48 @@ import TopHeader from '~/components/ui/layouts/top-header';
 import {getCategories} from './add-product-megamenu-server';
 import {json} from '@remix-run/server-runtime';
 
-export interface CategoriesType {
-  id: number;
-  title: string;
-  identifier: string;
-  child_categories?: CategoriesType[];
-}
+// export interface CategoriesType {
+//   id: number;
+//   title: string;
+//   identifier: string;
+//   child_categories?: CategoriesType[];
+// }
 
-export async function loader() {
-  try {
-    const categories = await getCategories();
-    return json({categories});
-  } catch (error) {
-    return json({categories: []});
-  }
-}
+// export async function loader() {
+//   try {
+//     const categories = await getCategories();
+//     return json({categories});
+//   } catch (error) {
+//     return json({categories: []});
+//   }
+// }
 
 /**
  * @description layout for protected page
  */
 
-export default function PublicPageLayout() {
-  const {categories} = useLoaderData<typeof loader>();
+// export default function PublicPageLayout() {
+//   const {categories} = useLoaderData<typeof loader>();
 
-  return (
-    <Layout categories={categories}>
-      <Outlet />
-    </Layout>
-  );
-}
+//   return (
+//     <Layout categories={categories}>
+//       <Outlet />
+//     </Layout>
+//   );
+// }
 
 const Layout = ({
   children,
-  categories,
-}: {
+}: // categories,
+{
   children: any;
-  categories: CategoriesType[];
+  // categories: CategoriesType[];
 }) => {
   return (
     <>
       <header>
         <TopHeader />
-        <BottomHeader categories={categories} />
+        {/* <BottomHeader categories={categories} /> */}
       </header>
       {children}
       <footer className="mt-12">
