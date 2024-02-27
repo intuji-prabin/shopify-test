1a988ab09a99e6a5b0f7c69b16d83299a9a424b6
Salinakasa <salina.kansakar@webo.digital>
2024-02-27 16:42:45 +0545

    feat: maping of product-det

---
+++ app/routes/_app.$mainCategory.$categoryId.$subCategoryId.$productSlug/product.server.ts
@@ -0,0 +1,30 @@
+import {ENDPOINT} from '~/lib/constants/endpoint.constant';
+
+export async function getProductDetails(customerId: string, handle: string) {
+  try {
+    const results: any = await fetch(
+      // `${ENDPOINT.PRODUCT.GET_PRODUCT}/${customerId}/${handle}`,
+      `https://processors-fatty-dvds-destroyed.trycloudflare.com/api/product/${customerId}/${handle}`,
+      {
+        method: 'GET',
+      },
+    );
+    const response = await results.json();
+    // if (!results.status) {
+    //   throw new Response(results.message, {
+    //     status: 404,
+    //   });
+    // }
+    if (response?.errors) {
+      throw new Error('Something went wrong');
+    }
+    if (!response?.status) {
+      throw new Error(response?.message);
+    }
+    return response.payload;
+  } catch (error) {
+    throw new Error(
+      'Oops! Something went wrong. Please hold tight and try again in a little while. Thank you for your understanding.',
+    );
+  }
+}
