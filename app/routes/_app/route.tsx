cc9b46941798613ca6bd575edb51da8a0138a358
Salinakasa <salina.kansakar@webo.digital>
2024-03-01 16:29:40 +0545

    feat: cartCount

--- app/routes/_app/route.tsx
+++ app/routes/_app/route.tsx
@@ -7,31 +7,31 @@ import {
 import BottomHeader from '~/components/ui/layouts/bottom-header';
 import DesktopFooter from '~/components/ui/layouts/desktopFooter';
 import TopHeader from '~/components/ui/layouts/top-header';
-import {Payload, getCagetoryList, getCategories} from './app.server';
-import {ActionFunctionArgs, json} from '@remix-run/server-runtime';
+import { Payload, getCagetoryList, getCategories } from './app.server';
+import { ActionFunctionArgs, json } from '@remix-run/server-runtime';
 import {
   getMessageSession,
   messageCommitSession,
   setErrorMessage,
 } from '~/lib/utils/toast-session.server';
-import {useMediaQuery} from '~/hooks/useMediaQuery';
+import { useMediaQuery } from '~/hooks/useMediaQuery';
 import MobileNav from '~/components/ui/layouts/elements/mobile-navbar/mobile-nav';
-import {getCategoryList} from '../_app.categories/route';
-import {isAuthenticate} from '~/lib/utils/auth-session.server';
-import {CustomerData} from '../_public.login/login.server';
-import {getUserDetails} from '~/lib/utils/user-session.server';
-import {HamburgerMenuProvider} from '~/components/ui/layouts/elements/HamburgerMenuContext';
+import { getCategoryList } from '../_app.categories/route';
+import { isAuthenticate } from '~/lib/utils/auth-session.server';
+import { CustomerData } from '../_public.login/login.server';
+import { getUserDetails } from '~/lib/utils/user-session.server';
+import { HamburgerMenuProvider } from '~/components/ui/layouts/elements/HamburgerMenuContext';
 
-export async function loader({request, context}: ActionFunctionArgs) {
+export async function loader({ request, context }: ActionFunctionArgs) {
   await isAuthenticate(context);
 
-  const {userDetails} = await getUserDetails(request);
+  const { userDetails } = await getUserDetails(request);
   const categories = await getCagetoryList(context);
   const messageSession = await getMessageSession(request);
   if (!categories) {
     setErrorMessage(messageSession, 'Category not found');
     return json(
-      {categories: [], userDetails},
+      { categories: [], userDetails },
       {
         headers: {
           'Set-Cookie': await messageCommitSession(messageSession),
@@ -39,14 +39,15 @@ export async function loader({request, context}: ActionFunctionArgs) {
       },
     );
   }
-  return json({categories, userDetails});
+  return json({ categories, userDetails });
 }
 
 export default function PublicPageLayout() {
-  const {categories, userDetails} = useLoaderData<typeof loader>();
+  const { categories, userDetails } = useLoaderData<typeof loader>();
+  const cartCount = 2001;
 
   return (
-    <Layout categories={categories} userDetails={userDetails}>
+    <Layout categories={categories} cartCount={cartCount} userDetails={userDetails}>
       <Outlet />
     </Layout>
   );
@@ -56,30 +57,30 @@ const Layout = ({
   children,
   categories,
   userDetails,
+  cartCount
 }: {
   children: React.ReactNode;
   categories: any;
   userDetails: CustomerData;
+  cartCount: number;
 }) => {
   const matches = useMediaQuery('(min-width: 768px)');
   return (
-    <>
-      <HamburgerMenuProvider>
-        {matches ? (
-          <header>
-            <TopHeader userDetails={userDetails} />
-            <BottomHeader categories={categories} />
-          </header>
-        ) : (
-          <MobileNav />
-        )}
-        <div className="mb-12">{children}</div>
+    <HamburgerMenuProvider>
+      {matches ? (
+        <header>
+          <TopHeader cartCount={cartCount} userDetails={userDetails} />
+          <BottomHeader categories={categories} />
+        </header>
+      ) : (
+        <MobileNav />
+      )}
+      <div className="mb-12">{children}</div>
 
-        <footer>
-          <DesktopFooter />
-        </footer>
-      </HamburgerMenuProvider>
-    </>
+      <footer>
+        <DesktopFooter />
+      </footer>
+    </HamburgerMenuProvider>
   );
 };
 
