3c7235bf8564cf6c260046ed8544c83b936bb913
Salinakasa <salina.kansakar@webo.digital>
2024-03-28 12:17:12 +0545

    Fixed the remove, added detail info and worked on price of compare page

--- app/routes/_app.product-comparison.($product1).($product2).($product3).($product4)/comparison-main-wrapper.tsx
+++ app/routes/_app.product-comparison.($product1).($product2).($product3).($product4)/comparison-main-wrapper.tsx
@@ -1,10 +1,9 @@
-import { Link, useLocation } from '@remix-run/react';
+import { Link, useLocation, useNavigate } from '@remix-run/react';
+import CloseMenu from '~/components/icons/closeMenu';
 import { TooltipInfo } from '~/components/icons/orderStatus';
+import { Button } from '~/components/ui/button';
 import { PredictiveSearch } from '~/components/ui/predictive-search';
 import { DEFAULT_IMAGE } from '~/lib/constants/general.constant';
-import { ProductResponse } from './getProduct.server';
-import { Button } from '~/components/ui/button';
-import CloseMenu from '~/components/icons/closeMenu';
 
 export type finalProductResponse = {
   productResponse: {
@@ -15,8 +14,28 @@ export type finalProductResponse = {
   }
 }
 
+export type ProductFinalResponse = {
+  id: string;
+  handle: string;
+  title: string;
+  warranty: { value: string };
+  material: { value: string };
+  product_weight?: { value: string };
+  supplier: { value: string };
+  featuredImage: { url: string };
+  variants: {
+    moq: string;
+    sku: string;
+    id: string;
+  }
+  companyPrice: string;
+  currency: string;
+  defaultPrice: string;
+  vendor: string;
+};
+
 type productResponse = {
-  product: ProductResponse;
+  product: ProductFinalResponse;
 }
 
 export default function ComparisonWrapper(productResponse: finalProductResponse) {
@@ -27,16 +46,23 @@ export default function ComparisonWrapper(productResponse: finalProductResponse)
   const product4 = finalResponse?.product4;
 
   const location = useLocation();
+  const navigate = useNavigate();
 
   const clearSelectedProduct = (productId: string) => {
-    console.log("clicked", location.pathname)
-    console.log("productId", productId)
-
     const originalURL = location.pathname;
-    const updatedURL = originalURL.replace(/\/\d+\//, `/${productId}/`);
-    console.log("updatedURL", updatedURL)
-  }
+    let splitURL = originalURL.split('/');
 
+    // splitURL = splitURL.filter((item) => item !== productId);
+    // let resultURL = splitURL.join('/');
+    // navigate(resultURL);
+
+    let indexToRemove = splitURL.lastIndexOf(productId);
+    if (indexToRemove !== -1) {
+      splitURL.splice(indexToRemove, 1);
+    }
+    let resultURL = '/' + splitURL.slice(1).join('/');
+    navigate(resultURL);
+  }
   return (
     <div className="mt-6 overflow-x-auto bg-white">
       <div className='flex gap-6 p-6'>
@@ -68,7 +94,7 @@ export default function ComparisonWrapper(productResponse: finalProductResponse)
                 </div>
               </div>
               <h3 className="italic leading-[36px] text-lg md:text-[30px] font-bold text-[#252727]">
-                $1234
+                <span className='text-lg font-medium'>{product1?.product?.currency}</span>{product1?.product?.companyPrice}
               </h3>
               <p className="text-[14px] font-normal leading-4">
                 (Excl. GST)
@@ -93,19 +119,14 @@ export default function ComparisonWrapper(productResponse: finalProductResponse)
                 </div>
               </div>
               <h3 className="italic leading-[36px] text-lg md:text-[30px] font-bold text-[#252727]">
-                $1234
+                <span className='text-lg font-medium'>{product1?.product?.currency}</span>{product1?.product?.defaultPrice}
               </h3>
               <p className="text-[14px] font-normal leading-4">
                 (inc. GST)
               </p>
             </div>
           </div>
-          <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Filter Lens Dimensions</p>
-          <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">120 x 100 x 18 mm</p>
-          <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">On/Off Control</p>
-          <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">Automatic</p>
-          <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Operating Temperature</p>
-          <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">-5C to 55C</p>
+          <ProductDetailInfo warranty={product1?.product?.warranty?.value && product1?.product?.warranty?.value || "N/A"} material={product1?.product?.material?.value && product1?.product?.material?.value || "N/A"} weight={product1?.product?.product_weight?.value && product1?.product?.product_weight?.value || "N/A"} supplier={product1?.product?.supplier?.value && product1?.product?.supplier?.value || "N/A"} vendor={product1?.product?.vendor && product1?.product?.vendor || "N/A"} moq={product1?.product?.variants?.moq && product1?.product?.variants?.moq || "N/A"} sku={product1?.product?.variants?.sku && product1?.product?.variants?.sku || "N/A"} />
         </div>
         <div className='min-w-[290px] w-full max-w-[290px]'>
           {product2?.product ?
@@ -143,7 +164,7 @@ export default function ComparisonWrapper(productResponse: finalProductResponse)
                     </div>
                   </div>
                   <h3 className="italic leading-[36px] text-lg md:text-[30px] font-bold text-[#252727]">
-                    $1234
+                    <span className='text-lg font-medium'>{product2?.product?.currency}</span>{product2?.product?.companyPrice}
                   </h3>
                   <p className="text-[14px] font-normal leading-4">
                     (Excl. GST)
@@ -168,19 +189,14 @@ export default function ComparisonWrapper(productResponse: finalProductResponse)
                     </div>
                   </div>
                   <h3 className="italic leading-[36px] text-lg md:text-[30px] font-bold text-[#252727]">
-                    $1234
+                    <span className='text-lg font-medium'>{product2?.product?.currency}</span>{product2?.product?.defaultPrice}
                   </h3>
                   <p className="text-[14px] font-normal leading-4">
                     (inc. GST)
                   </p>
                 </div>
               </div>
-              <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Filter Lens Dimensions</p>
-              <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">120 x 100 x 18 mm</p>
-              <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">On/Off Control</p>
-              <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">Automatic</p>
-              <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Operating Temperature</p>
-              <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">-5C to 55C</p>
+              <ProductDetailInfo warranty={product2?.product?.warranty?.value && product2?.product?.warranty?.value || "N/A"} material={product2?.product?.material?.value && product2?.product?.material?.value || "N/A"} weight={product2?.product?.product_weight?.value && product2?.product?.product_weight?.value || "N/A"} supplier={product2?.product?.supplier?.value && product2?.product?.supplier?.value || "N/A"} vendor={product2?.product?.vendor && product2?.product?.vendor || "N/A"} moq={product2?.product?.variants?.moq && product2?.product?.variants?.moq || "N/A"} sku={product2?.product?.variants?.sku && product2?.product?.variants?.sku || "N/A"} />
             </> :
             <>
               <div className='flex items-center justify-center h-48 p-5 bg-grey-25'>
@@ -202,7 +218,7 @@ export default function ComparisonWrapper(productResponse: finalProductResponse)
                 <img src={product3?.product?.featuredImage?.url || DEFAULT_IMAGE?.IMAGE} alt="product3" className='object-contain h-full overflow-hidden' />
                 <Button
                   className="absolute top-2 right-2 bg-white max-h-5 max-w-5 md:max-h-8 md:max-w-8 p-[3px] md:p-2 hover:bg-white"
-                  onClick={() => clearSelectedProduct(product2?.product?.id)}
+                  onClick={() => clearSelectedProduct(product3?.product?.id)}
                 >
                   <CloseMenu fillColor="#0F1010" />
                 </Button>
@@ -231,7 +247,7 @@ export default function ComparisonWrapper(productResponse: finalProductResponse)
                     </div>
                   </div>
                   <h3 className="italic leading-[36px] text-lg md:text-[30px] font-bold text-[#252727]">
-                    $1234
+                    <span className='text-lg font-medium'>{product3?.product?.currency}</span>{product3?.product?.companyPrice}
                   </h3>
                   <p className="text-[14px] font-normal leading-4">
                     (Excl. GST)
@@ -256,19 +272,14 @@ export default function ComparisonWrapper(productResponse: finalProductResponse)
                     </div>
                   </div>
                   <h3 className="italic leading-[36px] text-lg md:text-[30px] font-bold text-[#252727]">
-                    $1234
+                    <span className='text-lg font-medium'>{product3?.product?.currency}</span>{product3?.product?.defaultPrice}
                   </h3>
                   <p className="text-[14px] font-normal leading-4">
                     (inc. GST)
                   </p>
                 </div>
               </div>
-              <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Filter Lens Dimensions</p>
-              <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">120 x 100 x 18 mm</p>
-              <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">On/Off Control</p>
-              <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">Automatic</p>
-              <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Operating Temperature</p>
-              <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">-5C to 55C</p>
+              <ProductDetailInfo warranty={product3?.product?.warranty?.value && product3?.product?.warranty?.value || "N/A"} material={product3?.product?.material?.value && product3?.product?.material?.value || "N/A"} weight={product3?.product?.product_weight?.value && product3?.product?.product_weight?.value || "N/A"} supplier={product3?.product?.supplier?.value && product3?.product?.supplier?.value || "N/A"} vendor={product3?.product?.vendor && product3?.product?.vendor || "N/A"} moq={product3?.product?.variants?.moq && product3?.product?.variants?.moq || "N/A"} sku={product3?.product?.variants?.sku && product3?.product?.variants?.sku || "N/A"} />
             </> :
             <>
               <div className='flex items-center justify-center h-48 p-5 bg-grey-25'>
@@ -290,7 +301,7 @@ export default function ComparisonWrapper(productResponse: finalProductResponse)
                 <img src={product4?.product?.featuredImage?.url || DEFAULT_IMAGE?.IMAGE} alt="product4" className='object-contain h-full overflow-hidden' />
                 <Button
                   className="absolute top-2 right-2 bg-white max-h-5 max-w-5 md:max-h-8 md:max-w-8 p-[3px] md:p-2 hover:bg-white"
-                  onClick={() => clearSelectedProduct(product2?.product?.id)}
+                  onClick={() => clearSelectedProduct(product4?.product?.id)}
                 >
                   <CloseMenu fillColor="#0F1010" />
                 </Button>
@@ -319,7 +330,7 @@ export default function ComparisonWrapper(productResponse: finalProductResponse)
                     </div>
                   </div>
                   <h3 className="italic leading-[36px] text-lg md:text-[30px] font-bold text-[#252727]">
-                    $1234
+                    <span className='text-lg font-medium'>{product4?.product?.currency}</span>{product4?.product?.companyPrice}
                   </h3>
                   <p className="text-[14px] font-normal leading-4">
                     (Excl. GST)
@@ -344,19 +355,14 @@ export default function ComparisonWrapper(productResponse: finalProductResponse)
                     </div>
                   </div>
                   <h3 className="italic leading-[36px] text-lg md:text-[30px] font-bold text-[#252727]">
-                    $1234
+                    <span className='text-lg font-medium'>{product4?.product?.currency}</span>{product4?.product?.defaultPrice}
                   </h3>
                   <p className="text-[14px] font-normal leading-4">
                     (inc. GST)
                   </p>
                 </div>
               </div>
-              <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Filter Lens Dimensions</p>
-              <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">120 x 100 x 18 mm</p>
-              <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">On/Off Control</p>
-              <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">Automatic</p>
-              <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Operating Temperature</p>
-              <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">-5C to 55C</p>
+              <ProductDetailInfo warranty={product3?.product?.warranty?.value && product3?.product?.warranty?.value || "N/A"} material={product3?.product?.material?.value && product3?.product?.material?.value || "N/A"} weight={product3?.product?.product_weight?.value && product3?.product?.product_weight?.value || "N/A"} supplier={product3?.product?.supplier?.value && product3?.product?.supplier?.value || "N/A"} vendor={product3?.product?.vendor && product3?.product?.vendor || "N/A"} moq={product3?.product?.variants?.moq && product3?.product?.variants?.moq || "N/A"} sku={product3?.product?.variants?.sku && product3?.product?.variants?.sku || "N/A"} />
             </> :
             <>
               <div className='flex items-center justify-center h-48 p-5 bg-grey-25'>
@@ -372,6 +378,42 @@ export default function ComparisonWrapper(productResponse: finalProductResponse)
             </>}
         </div>
       </div>
-    </div >
+    </div>
   );
+}
+
+
+export function ProductDetailInfo({ warranty, material, weight, supplier, vendor, moq, sku }: { warranty: string, material: string, weight: string, supplier: string, vendor: string, moq: string, sku: string }) {
+  return (
+    <>
+      <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Warranty</p>
+      <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">
+        {warranty}
+      </p>
+      <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Material</p>
+      <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">
+        {material}
+      </p>
+      <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Product Weight</p>
+      <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">
+        {weight}
+      </p>
+      <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Supplier</p>
+      <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">
+        {supplier}
+      </p>
+      <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Vendor</p>
+      <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">
+        {vendor}
+      </p>
+      <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">Minimum Order Quantity</p>
+      <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">
+        {moq}
+      </p>
+      <p className="px-6 py-3 text-lg font-semibold leading-6 text-grey-900 bg-primary-50">SKU</p>
+      <p className="px-6 py-3 text-lg font-medium leading-6 text-grey-900">
+        {sku}
+      </p>
+    </>
+  )
 }
