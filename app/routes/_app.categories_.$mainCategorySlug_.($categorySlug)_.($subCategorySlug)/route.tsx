2d11abc3e6b481c966a917be6bb00a600b1abfdc
Salinakasa <salina.kansakar@webo.digital>
2024-04-16 07:56:28 +0545

    Filter structurwe

--- app/routes/_app.categories_.$mainCategorySlug_.($categorySlug)_.($subCategorySlug)/route.tsx
+++ app/routes/_app.categories_.$mainCategorySlug_.($categorySlug)_.($subCategorySlug)/route.tsx
@@ -1,14 +1,15 @@
-import { NavLink, json, useLoaderData } from "@remix-run/react";
-import { LoaderFunctionArgs } from "@remix-run/server-runtime";
-import { useCallback, useEffect, useState } from "react";
-import { BackButton } from "~/components/ui/back-button";
-import { Breadcrumb, BreadcrumbItem } from "~/components/ui/breadcrumb";
-import { Button } from "~/components/ui/button";
-import { Separator } from "~/components/ui/separator";
-import { Routes } from "~/lib/constants/routes.constent";
-import { isAuthenticate } from "~/lib/utils/auth-session.server";
+import { NavLink, json, useLoaderData } from '@remix-run/react';
+import { LoaderFunctionArgs } from '@remix-run/server-runtime';
+import { useCallback, useEffect, useState } from 'react';
+import { BackButton } from '~/components/ui/back-button';
+import { Breadcrumb, BreadcrumbItem } from '~/components/ui/breadcrumb';
+import { Button } from '~/components/ui/button';
+import { Separator } from '~/components/ui/separator';
+import { Routes } from '~/lib/constants/routes.constent';
+import { isAuthenticate } from '~/lib/utils/auth-session.server';
 import useEmblaCarousel, { EmblaCarouselType } from 'embla-carousel-react';
-import { getCategoryList } from "../_app.categories/route";
+import { getCategoryList } from '../_app.categories/route';
+import { LeftArrow } from '~/components/icons/left';
 
 export async function loader({ params, context, request }: LoaderFunctionArgs) {
     await isAuthenticate(context);
@@ -24,8 +25,11 @@ const linkStyles =
     'text-center basis-full border-b-2 inline-block duration-300 border-b-grey-50 cursor-pointer bg-grey-50 uppercase text-lg italic font-bold leading-6 text-grey-500 py-3 px-5 hover:bg-none';
 
 const route = () => {
-    const { mainCategorySlug, categorySlug, subCategorySlug, categories } = useLoaderData<typeof loader>();
-    const backTitle = subCategorySlug ? subCategorySlug?.split('-').join(' ') : categorySlug?.split('-').join(' ');
+    const { mainCategorySlug, categorySlug, subCategorySlug, categories } =
+        useLoaderData<typeof loader>();
+    const backTitle = subCategorySlug
+        ? subCategorySlug?.split('-').join(' ')
+        : categorySlug?.split('-').join(' ');
 
     const matchingCategory = categories
         .map((category) => {
@@ -37,8 +41,7 @@ const route = () => {
                 : null;
         })
         .filter((category) => category !== null)[0];
-    console.log("matchingCategory", matchingCategory);
-    const finalCategory = subCategorySlug ? matchingCategory?.subCategory : matchingCategory?.child_categories;
+    console.log('matchingCategory', matchingCategory);
 
     // For carousel at the top
     const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
@@ -79,19 +82,23 @@ const route = () => {
         <section className="container">
             <div className="flex flex-wrap justify-between pt-6">
                 <div>
-                    <BackButton className="capitalize" title={backTitle ?? "back"} />
+                    <BackButton className="capitalize" title={backTitle ?? 'back'} />
                     <Breadcrumb>
                         <BreadcrumbItem href={Routes.CATEGORIES} className="capitalize">
                             {mainCategorySlug?.split('-').join(' ')}
                         </BreadcrumbItem>
-                        <BreadcrumbItem href={subCategorySlug ? Routes.CATEGORIES : "#"} className={`capitalize ${!subCategorySlug && "text-grey-800 pointer-events-none"}`}>
+                        <BreadcrumbItem
+                            href={subCategorySlug ? Routes.CATEGORIES : '#'}
+                            className={`capitalize ${!subCategorySlug && 'text-grey-800 pointer-events-none'
+                                }`}
+                        >
                             {categorySlug?.split('-').join(' ')}
                         </BreadcrumbItem>
-                        {subCategorySlug &&
+                        {subCategorySlug && (
                             <BreadcrumbItem className="capitalize text-grey-800">
                                 {subCategorySlug?.split('-').join(' ')}
                             </BreadcrumbItem>
-                        }
+                        )}
                     </Breadcrumb>
                 </div>
                 <div>
@@ -103,34 +110,84 @@ const route = () => {
                 <div className="embla">
                     <div className="overflow-x-hidden embla__viewport" ref={emblaRef}>
                         <div className="flex gap-3 py-4 embla__container">
-                            {finalCategory.map((subCategory) =>
-                                subCategory.child_categories.map((childCategory, index) => (
-                                    <div
-                                        className="max-w-full min-w-0 flex-autoCustom embla__slide"
-                                        key={childCategory.id}
-                                    >
-                                        <NavLink
-                                            to={`/category/${matchingCategory.identifier}/${subCategory?.identifier}/${childCategory?.identifier}`}
-                                            data-index={index}
-                                            className={({ isActive, isPending }) =>
-                                                isPending
-                                                    ? `active__tab ${linkStyles}`
-                                                    : isActive
-                                                        ? `active__tab ${linkStyles}`
-                                                        : linkStyles
-                                            }
+                            {subCategorySlug
+                                ? matchingCategory?.subCategory.map((subCategory) =>
+                                    subCategory.child_categories.map((childCategory, index) => (
+                                        <div
+                                            className="max-w-full min-w-0 flex-autoCustom embla__slide"
+                                            key={childCategory.id}
                                         >
-                                            {childCategory.title}
-                                        </NavLink>
-                                    </div>
-                                )),
-                            )}
+                                            <NavLink
+                                                to={`/categories/${matchingCategory.identifier}/${subCategory?.identifier}/${childCategory?.identifier}`}
+                                                data-index={index}
+                                                className={({ isActive, isPending }) =>
+                                                    isPending
+                                                        ? `active__tab ${linkStyles}`
+                                                        : isActive
+                                                            ? `active__tab ${linkStyles}`
+                                                            : linkStyles
+                                                }
+                                            >
+                                                {childCategory.title}
+                                            </NavLink>
+                                        </div>
+                                    )),
+                                )
+                                : matchingCategory?.child_categories
+                                    .filter(
+                                        (subCategory) =>
+                                            subCategory.child_categories.length === 0,
+                                    )
+                                    .map((subCategory, index: number) => (
+                                        <div
+                                            className="max-w-full min-w-0 flex-autoCustom embla__slide"
+                                            key={subCategory.id}
+                                        >
+                                            <NavLink
+                                                to={`/categories/${matchingCategory.identifier}/${subCategory?.identifier}`}
+                                                data-index={index}
+                                                className={({ isActive, isPending }) =>
+                                                    isPending
+                                                        ? `active__tab ${linkStyles}`
+                                                        : isActive
+                                                            ? `active__tab ${linkStyles}`
+                                                            : linkStyles
+                                                }
+                                            >
+                                                {subCategory.title}
+                                            </NavLink>
+                                        </div>
+                                    ))}
                         </div>
                     </div>
+                    <button
+                        className={`absolute z-10 flex items-center justify-center w-6 h-auto -translate-y-1/2 cursor-pointer -left-3 embla__button embla__next aspect-square top-1/2 bg-white shadow-md ${prevBtnDisabled ? 'hidden' : 'flex'
+                            }`}
+                        onClick={scrollPrev}
+                        disabled={prevBtnDisabled}
+                    >
+                        <LeftArrow height={10} fill="#000" />
+                    </button>
+                    <button
+                        className={`absolute z-10 items-center justify-center w-6 h-auto rotate-180 -translate-y-1/2 cursor-pointer -right-3 embla__button embla__prev aspect-square top-1/2 bg-white shadow-md ${nextBtnDisabled ? 'hidden' : 'flex'
+                            }`}
+                        onClick={scrollNext}
+                        disabled={nextBtnDisabled}
+                    >
+                        <LeftArrow height={10} fill="#000" />
+                    </button>
                 </div>
+            </div>
+            <div className="grid grid-cols-1 gap-6 xl:grid-cols-4">
+                <div className="xl:col-span-1">
+                    <div className="sticky top-[100px] bg-neutral-white">
+                        hello filter
+                    </div>
+                </div>
+                <div className="xl:col-start-2 xl:col-end-5">hello right</div>
             </div>
         </section>
     );
-}
+};
 
 export default route;
