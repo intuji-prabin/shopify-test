620a805c975eacf94f279b82f2ed917d9fb0dbf5
Salinakasa <salina.kansakar@webo.digital>
2024-05-02 14:05:32 +0545

    feat: profile section of the home page

--- app/components/ui/cta-home.tsx
+++ app/components/ui/cta-home.tsx
@@ -1,17 +1,17 @@
-import {Link} from '@remix-run/react';
-import Receipt from '../icons/receipt';
+import { Link } from '@remix-run/react';
+import { Routes } from '~/lib/constants/routes.constent';
 import ArrowRight from '../icons/arrowRight';
-import Distance from '../icons/distance';
 import Bell from '../icons/bell';
-import {Separator} from './separator';
-import {Routes} from '~/lib/constants/routes.constent';
+import Receipt from '../icons/receipt';
+import { TrackAnOrderButton } from './layouts/elements/track-an-order-dialog';
+import { Separator } from './separator';
 
 const CtaHome = () => {
   return (
     <section className="container">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-3 gap-x-3 lg:gap-x-6 place-content-center">
         <Link
-          to={Routes.CART_LIST}
+          to={Routes.PLACE_AN_ORDER}
           className="cta__btn pr-6 pl-4 py-4 lg:py-6 lg:pl-6 lg:pr-8 text-lg italic font-bold capitalize bg-secondary-500 lg:text-xl xl:text-2xl min-h-[88px] flex justify-between items-center"
         >
           <span className="flex items-center gap-1">
@@ -21,18 +21,7 @@ const CtaHome = () => {
             <ArrowRight />
           </span>
         </Link>
-        <Link
-          to=""
-          className="cta__btn pr-6 pl-4 py-4 lg:py-6 lg:pl-6 lg:pr-8 text-lg italic font-bold capitalize bg-primary-500 text-white lg:text-xl xl:text-2xl min-h-[88px] flex justify-between items-center"
-        >
-          <span className="flex items-center gap-1">
-            <Distance />
-            Track An Order
-          </span>
-          <span className="arrow__animation">
-            <ArrowRight fillColor="#ffffff" />
-          </span>
-        </Link>
+        <TrackAnOrderButton trackAnOrderHome={true} />
         <Link
           to={Routes.NOTIFICATION}
           className="cta__btn pr-6 pl-4 py-4 lg:py-6 lg:pl-6 lg:pr-8 text-lg italic font-bold capitalize bg-primary-500 text-white lg:text-xl xl:text-2xl min-h-[88px] flex justify-between items-center"
