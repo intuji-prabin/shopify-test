7c5d16d84493dcec92b8bc51e48bf1fc6091ba1c
Prabin Rai <prabin.rai@webo.digital>
2024-05-02 08:41:36 +0545

    feat: fix the merge issue on productTabs

--- app/routes/_app/route.tsx
+++ app/routes/_app/route.tsx
@@ -96,7 +96,8 @@ const fetchRoleDataWithTimeout = () => {
       const roleData: RoleData = {
         title: 'Admin',
         value: 'test',
-        permissions: [
+        permissions: 
+        [
           {
             id: 1,
             value: 'customer_login',
