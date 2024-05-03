6186eba44024762802e587b2d7eae530f3b4d081
Prabin Rai <prabin.rai@webo.digital>
2024-05-03 08:04:13 +0545

    feat: fix conflict on permission subscribe

--- app/routes/permissions.subscribe.ts
+++ app/routes/permissions.subscribe.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // routes/permissions-subscribe.ts
 
 import {LoaderFunction, LoaderFunctionArgs, json} from '@remix-run/server-runtime';
@@ -78,35 +77,6 @@ export async function loader({request}: LoaderFunctionArgs) {
 
     return function clear() {
       emitter2.off(EVENTS.PERMISSIONS_UPDATED.KEY, handle);
-=======
-// permissions.subscribe.ts
-
-import type { LoaderFunctionArgs } from '@remix-run/node';
-import { eventStream } from 'remix-utils/sse/server';
-import { EVENTS } from '~/lib/constants/events.contstent';
-import { emitter } from '~/lib/utils/emitter.server';
-import { getUserDetails } from '~/lib/utils/user-session.server';
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  // Fetch userDetails
-  const { userDetails } = await getUserDetails(request);
-  const permissions = userDetails?.meta?.user_role;
-  console.log('permissions', permissions);
-
-  // Create an event stream to send permissions data
-  return eventStream(request.signal, function setup(send) {
-    // Function to handle permissions update
-    async function handle() {
-      send({ event: EVENTS.PERMISSIONS_UPDATED.NAME, data: JSON.stringify(permissions) });
-    }
-
-    // Listen for permissions update event
-    emitter.on(EVENTS.PERMISSIONS_UPDATED.KEY, handle);
-
-    // Clear event listener when request is aborted
-    return function clear() {
-      emitter.off(EVENTS.PERMISSIONS_UPDATED.KEY, handle);
->>>>>>> 10f1729fc01ae0d3086de9761086caca8f9f3e7c
     };
   });
 }
