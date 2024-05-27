519f973c632ee9628408e11c74de68ddf5bbeebd
Prabin Rai <prabin.rai@webo.digital>
2024-05-27 16:20:08 +0545

    feat: added the notification badge on the home notifications header

--- app/components/ui/cta-home.tsx
+++ app/components/ui/cta-home.tsx
@@ -1,13 +1,16 @@
-import { Link } from '@remix-run/react';
-import { Routes } from '~/lib/constants/routes.constent';
-import { Can } from '~/lib/helpers/Can';
+import {Link} from '@remix-run/react';
+import {Routes} from '~/lib/constants/routes.constent';
+import {Can} from '~/lib/helpers/Can';
 import ArrowRight from '../icons/arrowRight';
 import Bell from '../icons/bell';
 import Receipt from '../icons/receipt';
-import { TrackAnOrderButton } from './layouts/elements/track-an-order-dialog';
-import { Separator } from './separator';
+import {TrackAnOrderButton} from './layouts/elements/track-an-order-dialog';
+import {Separator} from './separator';
+type PropType = {
+  totalNotificationCount: number;
+};
 
-const CtaHome = () => {
+const CtaHome: React.FC<PropType> = ({ totalNotificationCount }) => {
   return (
     <section className="container">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-3 gap-x-3 lg:gap-x-6 place-content-center">
@@ -29,10 +32,17 @@ const CtaHome = () => {
           to={Routes.NOTIFICATIONS_NEW}
           className="cta__btn pr-6 pl-4 py-4 lg:py-6 lg:pl-6 lg:pr-8 text-lg italic font-bold capitalize bg-primary-500 text-white lg:text-xl xl:text-2xl min-h-[88px] flex justify-between items-center"
         >
-          <span className="flex items-center gap-1">
-            <Bell />
+          <span className="relative flex items-center gap-1">
+            <div data-tooltip="Notifications" className="relative">
+              {/* Notification Badge */}
+              <div className="absolute bg-semantic-danger-500 h-[20px] min-w-[20px] rounded-full right-[4px] top-[-4px] flex items-center justify-center text-sm text-white font-medium">
+                {totalNotificationCount}
+              </div>
+              <Bell />
+            </div>
             Notifications
           </span>
+
           <span className="arrow__animation">
             <ArrowRight fillColor="#ffffff" />
           </span>
