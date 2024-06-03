734ed3ebfbfeb7a47e4d19549134d5984140de00
Kiran Kumal <kiran.kumal@webo.digital>
2024-06-03 10:45:29 +0545

    feat: implement impersonate feature

--- app/routes/_app.support_.allow-impersonate/route.tsx
+++ app/routes/_app.support_.allow-impersonate/route.tsx
@@ -1,16 +1,30 @@
 import {
   ActionFunctionArgs,
   LoaderFunctionArgs,
+  json,
 } from '@remix-run/server-runtime';
 import {BackButton} from '~/components/ui/back-button';
 import {Breadcrumb, BreadcrumbItem} from '~/components/ui/breadcrumb';
 import {useConditionalRender} from '~/hooks/useAuthorization';
-import {useFetch} from '~/hooks/useFetch';
-import {ENDPOINT} from '~/lib/constants/endpoint.constant';
 import {Routes} from '~/lib/constants/routes.constent';
 import {isAuthenticate} from '~/lib/utils/auth-session.server';
 import {getUserDetails} from '~/lib/utils/user-session.server';
-import {AllowImpersonateForm} from '~/routes/_app.support_.allow-impersonate/allow-impersonate-form';
+import {
+  AllowImpersonateForm,
+  ImpersonateFormFieldValidator,
+} from '~/routes/_app.support_.allow-impersonate/allow-impersonate-form';
+import {
+  getImpersonateStatus,
+  updateImpersonateStatus,
+} from '~/routes/_app.support_.allow-impersonate/allow-impoersonate.server';
+import {
+  isRouteErrorResponse,
+  useLoaderData,
+  useRouteError,
+} from '@remix-run/react';
+import {validationError} from 'remix-validated-form';
+import {AllowedHTTPMethods} from '~/lib/enums/api.enum';
+import {RouteError} from '~/components/ui/route-error';
 
 export async function loader({request, context}: LoaderFunctionArgs) {
   await isAuthenticate(context);
@@ -18,40 +32,72 @@ export async function loader({request, context}: LoaderFunctionArgs) {
   const {userDetails} = await getUserDetails(request);
 
   const customerId = userDetails.id.split('/').pop() as string;
-  console.log(
-    'url',
-    `http://170.64.130.119:3000/api/impersonate/${customerId}`,
-  );
-  try {
-    const response = await useFetch({
-      url: `http://170.64.130.119:3000/api/impersonate/${customerId}`,
-    });
 
-    console.log('response', response);
-  } catch (error) {
-    console.log('error', error);
-  }
+  const {impersonateActive} = await getImpersonateStatus(customerId);
 
-  return null;
+  return json({isImpersonateActive: impersonateActive});
 }
 
-export async function action({request}: ActionFunctionArgs) {}
+export async function action({request, context}: ActionFunctionArgs) {
+  await isAuthenticate(context);
+
+  const {userDetails} = await getUserDetails(request);
+
+  const customerId = userDetails.id.split('/').pop() as string;
+
+  const formData = await request.formData();
+
+  const results = await ImpersonateFormFieldValidator.validate(formData);
+
+  if (results.error) {
+    return validationError(results.error);
+  }
+  const {reason} = results.data;
+
+  const action = formData.get('_action') as
+    | 'allow_impersonate'
+    | 'disallow_impersonate';
+
+  const body = JSON.stringify({impersonateReason: reason});
+
+  switch (action) {
+    case 'allow_impersonate':
+      return await updateImpersonateStatus({
+        customerId,
+        body,
+        request,
+        method: AllowedHTTPMethods.POST,
+      });
+    case 'disallow_impersonate':
+      return await updateImpersonateStatus({
+        customerId,
+        body,
+        request,
+        method: AllowedHTTPMethods.PUT,
+      });
+
+    default:
+      return null;
+  }
+}
 
 export default function AllowImpersonatePage() {
+  const {isImpersonateActive} = useLoaderData<typeof loader>();
+
   const shouldRender = useConditionalRender('allow_impersonation');
 
   return (
     shouldRender && (
       <section className="container">
         <div className=" pt-6 pb-4">
-          <BackButton title="Allow Impersonate" />
+          <BackButton title="Impersonate" />
           <Breadcrumb>
             <BreadcrumbItem href={Routes.SUPPORT}>Support</BreadcrumbItem>
             <BreadcrumbItem
               href={Routes.SUPPORT_ALLOW_IMPERSONATE}
               className="text-grey-900"
             >
-              Allow Impersonate
+              Impersonate
             </BreadcrumbItem>
           </Breadcrumb>
         </div>
@@ -62,8 +108,52 @@ export default function AllowImpersonatePage() {
           provides an efficient way to address problems without the need for
           physical presence.
         </p>
-        <AllowImpersonateForm />
+        <AllowImpersonateForm isImpersonateActive={isImpersonateActive} />
       </section>
     )
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <section className="container">
+        <div className=" pt-6 pb-4">
+          <BackButton title="Impersonate" />
+          <Breadcrumb>
+            <BreadcrumbItem href={Routes.SUPPORT}>Support</BreadcrumbItem>
+            <BreadcrumbItem
+              href={Routes.SUPPORT_ALLOW_IMPERSONATE}
+              className="text-grey-900"
+            >
+              Impersonate
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </div>
+        <RouteError />
+      </section>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <section className="container">
+        <div className=" pt-6 pb-4">
+          <BackButton title="Impersonate" />
+          <Breadcrumb>
+            <BreadcrumbItem href={Routes.SUPPORT}>Support</BreadcrumbItem>
+            <BreadcrumbItem
+              href={Routes.SUPPORT_ALLOW_IMPERSONATE}
+              className="text-grey-900"
+            >
+              Impersonate
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </div>
+        <RouteError errorMessage={error.message} />
+      </section>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }
