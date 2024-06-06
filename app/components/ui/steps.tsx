2b8de767902117ec065a29367359da6cd33f91b6
Salinakasa <salina.kansakar@webo.digital>
2024-06-06 12:25:48 +0545

    responsiveness of pages

--- app/components/ui/steps.tsx
+++ app/components/ui/steps.tsx
@@ -8,7 +8,7 @@ interface StepsType {
 export const Steps = ({ children, simple = true, className }: StepsType) => {
   return (
     <ul
-      className={`steps flex before:content-[""] before:bg-grey-50 before:w-full before:h-0.5 before:inline-block before:absolute relative before:top-8 before:z-0 ${className} ${simple ? 'justify-between before:top-[15px]' : ''
+      className={`steps flex flex-col gap-y-10 md:flex-row before:content-[""] before:bg-grey-50 before:md:w-full before:md:h-0.5 before:inline-block before:absolute relative before:md:top-8 before:top-0 before:w-0.5 before:h-full before:left-1/2 before:md:left-[unset] before:z-0 ${className} ${simple ? 'justify-between before:top-[15px]' : ''
         }`}
     >
       {children}
