83244b1ffa33a7b848792bdfb974a148ebb9b413
Salinakasa <salina.kansakar@webo.digital>
2024-07-02 16:45:15 +0545

    feat: removed condition for single page on pagination and added link to image in predictive searches

--- app/components/ui/predictive-search.tsx
+++ app/components/ui/predictive-search.tsx
@@ -202,11 +202,17 @@ function renderProductItem(
           key={product.id}
         >
           <div className="size-14">
-            <img
-              src={product?.featuredPriceImageUrl}
-              alt="product"
-              className="object-cover object-center size-full"
-            />
+            <Link
+              prefetch="intent"
+              to={`/product/${product.handle}`}
+              onClick={handleClose}
+            >
+              <img
+                src={product?.featuredPriceImageUrl}
+                alt="product"
+                className="object-cover object-center size-full"
+              />
+            </Link>
           </div>
           <figcaption className="w-[calc(100%_-_72px)]">
             <Link
@@ -232,11 +238,17 @@ function renderProductItem(
         >
           <div className="flex flex-wrap items-center gap-3 sm:w-4/6">
             <div className="size-16">
-              <img
-                src={product?.featuredPriceImageUrl}
-                alt="product"
-                className="object-contain object-center size-full"
-              />
+              <Link
+                prefetch="intent"
+                to={`/product/${product.handle}`}
+                onClick={handleClose}
+              >
+                <img
+                  src={product?.featuredPriceImageUrl}
+                  alt="product"
+                  className="object-contain object-center size-full"
+                />
+              </Link>
             </div>
             <div className="w-[calc(100%_-_76px)]">
               <p className="text-sm text-primary-500">
@@ -352,11 +364,17 @@ function renderProductItem(
         >
           <div className="flex flex-wrap items-center gap-3 sm:w-4/6">
             <div className="size-16">
-              <img
-                src={product?.featuredPriceImageUrl}
-                alt="product"
-                className="object-contain object-center size-full"
-              />
+              <Link
+                prefetch="intent"
+                to={`/product/${product.handle}`}
+                onClick={handleClose}
+              >
+                <img
+                  src={product?.featuredPriceImageUrl}
+                  alt="product"
+                  className="object-contain object-center size-full"
+                />
+              </Link>
             </div>
             <div className="w-[calc(100%_-_76px)]">
               <p className="text-sm text-primary-500">
@@ -463,11 +481,17 @@ function renderProductItem(
           className="flex flex-wrap items-center space-x-4"
         >
           <div className="size-14">
-            <img
-              src={product?.featuredPriceImageUrl}
-              alt="product"
-              className="object-cover object-center size-full"
-            />
+            <Link
+              prefetch="intent"
+              to={product.id}
+              onClick={handleClose}
+            >
+              <img
+                src={product?.featuredPriceImageUrl}
+                alt="product"
+                className="object-cover object-center size-full"
+              />
+            </Link>
           </div>
           <figcaption className="w-[calc(100%_-_72px)]">
             <Link
@@ -497,11 +521,17 @@ function renderProductItem(
         >
           <div className="flex flex-wrap items-center gap-3 sm:w-2/5">
             <div className="size-16">
-              <img
-                src={product?.featuredPriceImageUrl}
-                alt="product"
-                className="object-contain object-center size-full"
-              />
+              <Link
+                prefetch="intent"
+                to={`/product/${product.handle}`}
+                onClick={handleClose}
+              >
+                <img
+                  src={product?.featuredPriceImageUrl}
+                  alt="product"
+                  className="object-contain object-center size-full"
+                />
+              </Link>
             </div>
             <div className="w-[calc(100%_-_76px)]">
               <p className="text-sm text-primary-500">
@@ -634,11 +664,17 @@ function renderProductItem(
           key={product.id}
         >
           <div className="size-14">
-            <img
-              src={product?.featuredPriceImageUrl}
-              alt="product"
-              className="object-cover object-center size-full"
-            />
+            <Link
+              prefetch="intent"
+              to={`/product/${product.handle}`}
+              onClick={handleClose}
+            >
+              <img
+                src={product?.featuredPriceImageUrl}
+                alt="product"
+                className="object-cover object-center size-full"
+              />
+            </Link>
           </div>
           <figcaption className="w-[calc(100%_-_72px)]">
             <Link
