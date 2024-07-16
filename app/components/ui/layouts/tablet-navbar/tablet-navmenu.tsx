edeba729fe0d8359610a473c32bf93a8be344e32
Salinakasa <salina.kansakar@webo.digital>
2024-07-16 12:25:49 +0545

    feat: updated home page

--- app/components/ui/layouts/tablet-navbar/tablet-navmenu.tsx
+++ app/components/ui/layouts/tablet-navbar/tablet-navmenu.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
-import { TabletHamburger } from '~/components/icons/orderStatus';
 import CloseMenu from '~/components/icons/closeMenu';
+import { TabletHamburger } from '~/components/icons/orderStatus';
 import { useOutsideClick } from '~/hooks/useOutsideClick';
 import { Button } from '../../button';
 import { useHamburgerMenu } from '../elements/HamburgerMenuContext';
@@ -17,12 +17,13 @@ export default function TabletNavmenu({ profileName, profileImage }: { profileNa
 
   return (
     <>
-      <figure
+      {/* <figure
         className="flex xl:hidden border border-[#313535] bg-transparent max-w-10 max-h-10 hover:bg-transparent p-2"
         onClick={() => toggleMenu(!isOpen)}
       >
         <TabletHamburger />
-      </figure>
+      </figure> */}
+      <p className='font-medium text-secondary-500 italic border border-solid border-secondary-500 px-2.5 py-1'>MENU</p>
       <div className={`${isOpen ? "block xl:hidden" : "hidden"} absolute top-0 z-30 left-0 right-0 bg-black/80`}>
         <div className="w-40 bg-primary-500 min-w-[310px] h-screen" ref={tabletSectionRef}>
           <div className="flex items-center justify-between p-4 bg-grey-900 tab-header">
